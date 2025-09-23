e7c3965e84a0670adb365f0d3fcaed9963dedd05
Anna <jonyhoran@gmail.com>
2025-09-23 22:53:12 +0300

    PEP8 fix

--- app/urls.py
+++ app/urls.py
@@ -5,7 +5,9 @@ app_name = "app"
 
 urlpatterns = [
     path("departments/", views.departments, name="departments"),
-    path("departments/<int:id>/", views.departments_details, name="departments_details"),
+    path("departments/<int:id>/", views.departments_details,
+         name="departments_details"),
     path("programs/", views.programs, name="programs"),
-    path("programs/<int:id>/", views.programs_details, name="programs_details"),
+    path("programs/<int:id>/", views.programs_details,
+         name="programs_details"),
 ]
